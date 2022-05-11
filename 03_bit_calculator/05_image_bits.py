b286653d07a7cc35606be83da26eb20bffa0e5d4
toem1208 <101071945+toem1208@users.noreply.github.com>
2022-05-12 09:38:58 +1200

    Add files via upload

---
+++ 03_bit_calculator/05_image_bits.py
@@ -0,0 +1,50 @@
+
+# checks input is a number more than a given value
+def num_check(question, low):
+
+    valid = False
+    while not valid:
+
+        error = "Please enter a number that is more than (or equal to) {}".format(low)
+
+        try:
+
+            # ask user to enter a number 
+            response = int(input(question))
+
+            # checks number is more than zero
+            if response >= low:
+                return response
+
+            # outputs error if input is invalid
+            else:
+                    print(error)
+                    print()
+
+        except ValueError:
+            print(error)
+
+# finds # of bits for 24 bit colour 
+def image_bits():
+
+    # get width and hieght...
+    image_width = num_check("Image width? ", 1)
+    image_height = num_check("Image height? ", 1)
+
+    # calculate # of pixels  
+    num_pixels = image_width * image_height
+
+    # calculate # bits (24 x num pixels)
+    num_bits = num_pixels * 24
+
+    # output answer with working
+    print()
+    print("# of pixels = {} x {} = {}".format(image_height,
+                                              image_width, num_pixels))
+    print("# bits = {} x 24 = {}".format(num_pixels, num_bits))
+    print()
+
+    return ""
+
+# main routine goes here
+image_bits()
